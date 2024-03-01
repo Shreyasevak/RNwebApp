8aeb1a12e997d42dd334ab1f00af17f480f90740
“shreya” <shreya.nyusoft@gmail.com>
2024-03-01 21:16:00 +0530

    first commit

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -5,13 +5,14 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
-
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
-
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import { render, waitFor, } from '@testing-library/react-native';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('renders correctly', async () => {
+  const { toJSON, } = render(<App />);
+
+  await waitFor(() => {
+
+    expect(toJSON).toMatchSnapshot()
+  });
 });
